a899a34ed09114596c60f5ae5810b0ba7ef0e14d
Vivek Patel <vnp229@gmail.com>
2016-03-09 16:29:28 -0600

    created basic player and dealer class

--- bin/blackjack.rb
+++ bin/blackjack.rb
@@ -1,6 +1,35 @@
 require_relative '../lib/card'
 require_relative '../lib/deck'
 
+class Player
+
+  #has a turn
+  attr_accessor :name, :hand
+  def initialize(name = 'Player')
+    @name = name
+    @hand = []
+  end
+
+  def display_hand
+    @hand.each do |card|
+      puts "#{card.value} of #{card.suit}"
+    end
+  end
+
+
+end
+
+class Dealer
+  attr_accessor :hand
+  def initialize
+    @hand = []
+  end
+
+  def display_one_card
+    puts "#{@hand[0].value} of #{@hand[0].suit}"
+  end
+
+end
 class Game
   def initialize
     @deck = Deck.new
@@ -8,8 +37,16 @@ class Game
   end
 
   def play
-    card = @deck.deal
-    puts card.value + card.suit
+    p1 = Player.new
+    d1 = Dealer.new
+
+    2.times do
+      p1.hand.push(@deck.deal)
+      d1.hand.push(@deck.deal)
+    end
+
+    p1.display_hand
+    d1.display_one_card
   end
 
 end
--- lib/card.rb
+++ lib/card.rb
@@ -4,4 +4,13 @@ class Card
     @suit = suit.to_s
     @value = value.to_s
   end
+
+  def rank
+    case @value
+    when "K","Q","J" then @rank=10
+    when "A" then @rank=[1,11]
+    else
+      @rank = @value.to_i
+    end
+  end
 end
