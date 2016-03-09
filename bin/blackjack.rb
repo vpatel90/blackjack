56c538687c4043044701d7d1eb2544b40ae36d3d
Vivek Patel <vnp229@gmail.com>
2016-03-09 17:00:21 -0600

    added check_dealer_blackjack_fnction

--- bin/blackjack.rb
+++ bin/blackjack.rb
@@ -2,34 +2,48 @@ require_relative '../lib/card'
 require_relative '../lib/deck'
 
 class Player
-
-  #has a turn
-  attr_accessor :name, :hand
+  attr_accessor :name, :hand, :points
   def initialize(name = 'Player')
     @name = name
     @hand = []
+    @points = 0
   end
 
   def display_hand
     @hand.each do |card|
-      puts "#{card.value} of #{card.suit}"
+      puts "Player > #{card.value} of #{card.suit}"
     end
   end
 
+  def add_points
+    @points += @hand.last.rank
+  end
+
+  def turn
+    puts "Would you like to (H)it or (S)tay"
+  end
 
 end
 
 class Dealer
-  attr_accessor :hand
+  attr_accessor :hand, :points
   def initialize
     @hand = []
+    @points = 0
   end
 
   def display_one_card
-    puts "#{@hand[0].value} of #{@hand[0].suit}"
+    puts "Dealer > #{@hand[0].value} of #{@hand[0].suit}"
+    puts "Dealer > #{@hand[1].value} of #{@hand[1].suit}" ##Remove later
   end
 
+  def add_points
+    @points += @hand.last.rank
+  end
+
+
 end
+
 class Game
   def initialize
     @deck = Deck.new
@@ -37,18 +51,44 @@ class Game
   end
 
   def play
-    p1 = Player.new
-    d1 = Dealer.new
+    @p1 = Player.new
+    @d1 = Dealer.new
+    deal_cards
+    check_dealer_blackjack
 
-    2.times do
-      p1.hand.push(@deck.deal)
-      d1.hand.push(@deck.deal)
-    end
+    @p1.turn
 
-    p1.display_hand
-    d1.display_one_card
+    @p1.display_hand
+    puts @p1.points
+    @d1.display_one_card
+    puts @d1.points
+
+
   end
 
+  def deal_cards
+    2.times do
+      @p1.hand.push(@deck.deal)
+      @p1.add_points
+      @d1.hand.push(@deck.deal)
+      @d1.add_points
+    end
+  end
+
+  def check_dealer_blackjack
+    game_end if @d1.points == 21
+  end
+
+  def hit(player)
+    puts "#{player.name} hits"
+    player.hand.push(@deck.deal)
+    player.add_points
+  end
+
+  def game_end
+    puts "good game!"
+    exit
+  end
 end
 
 game = Game.new
--- lib/card.rb
+++ lib/card.rb
@@ -8,7 +8,7 @@ class Card
   def rank
     case @value
     when "K","Q","J" then @rank=10
-    when "A" then @rank=[1,11]
+    when "A" then @rank=11
     else
       @rank = @value.to_i
     end
